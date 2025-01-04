bc0844156da32cebcda171b7c792ee85ba94f80e
AstroBesat <128177174+AstroBesat-SoftW@users.noreply.github.com>
2025-01-04 19:19:02 +0300

    Create Process_olusturma.c

---
+++ C/Process_olusturma.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+//linux için 
+int main() {
+    pid_t pid = fork(); 
+
+    if (pid == 0) {
+      
+        char *args[] = {"/bin/ls", "-l", NULL}; 
+        execve("/bin/ls", args, NULL); 
+    } else if (pid > 0) {
+        
+        printf("Ebeveyn process: PID = %d, Çocuk PID = %d\n", getpid(), pid);
+    } else {
+        
+        perror("Fork hatası");
+    }
+    return 0;
+}
